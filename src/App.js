09d2458a98d56acdc2b67a2860d70e8a457d6ca4
love-jossan <lovedeepkamboj732@gmail.com>
2023-03-01 16:31:35 +0530

    latest code usestate

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 // import logo from './logo.svg';
-
-
+import Aboutus from './Aboutus';
 import './App.css';
 import Navbar from './Navbar';
 import Textform from './Textform';
 
-
 function App() {
   return (
     <>
-  
    <Navbar />
    <div className="container">
    <Textform />
+   </div>
+   <div className='container my-5'>
+<Aboutus/>
    </div>
 </>
   );
--- src/Textform.js
+++ src/Textform.js
@@ -1,24 +1,35 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 
 export default function Textform() {
   const [text, setText] = useState("enter text here");
-  const handleUp=()=>{
-console.log("enter the text")
-setText("change the text")
-}
- const handleChange=(event)=>{
-  setText(event.target.value)
+  const handleUp = () => {
+    let newText = text.toUpperCase();
+    console.log("enter the text")
 
-}
+    setText(newText)
+  }
+  const handleChange = (event) => {
+    setText(event.target.value)
+
+  }
+ const removeSpace=()=>{
+  let spaceText=text.split(/[ ]+/)
+  setText(spaceText.join(" "))
+ }
 
   return (
+    <>
     <div>
-   
-<div className="mb-3">
- <h1>Enter Text-{text}</h1>
-  <textarea className="form-control" value={text} onChange={handleChange} id="mybox" rows="3"></textarea>
-</div>
-<button className='btn btn-primary' onClick={handleUp}>Covert uppercase</button>
-    </div>
+        <div className="mb-3">
+          <h1>Enter Text </h1>
+          <textarea className="form-control" value={text} onChange={handleChange} id="mybox" rows="3"></textarea>
+        </div>
+        <button className='btn btn-primary' onClick={handleUp}>Covert uppercase
+        </button> <span><b>{text.split(" ").length}</b> word  and characters <b>{text.length}</b> </span>
+        <button className='btn btn-primary' onClick={removeSpace}>Remove Extra Space</button>
+    
+        </div>
+        </>
+  
   )
 }
