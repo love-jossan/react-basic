4cc80af352c4d82dbf13ddf5c4beb7a45e49971a
love-jossan <lovedeepkamboj732@gmail.com>
2023-03-02 16:00:15 +0530

    new code

--- src/Aboutus.js
+++ src/Aboutus.js
@@ -20,6 +20,7 @@ export default function Aboutus() {
         background:'black'
       });
       setBtn("White  Mode")
+
     }
   }
   return (
--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import Aboutus from './Aboutus';
 import './App.css';
 import Navbar from './Navbar';
 import Textform from './Textform';
-
+import { useState } from 'react';
 function App() {
+  const [mode, setMode] = useState("white");
+   const toggleMode=()=>{
+    if(mode==='white'){
+      setMode('dark')
+    }
+    else{
+      setMode('white')
+    }
+   }
   return (
     <>
-   <Navbar />
-   <div className="container">
+   <Navbar  mode={mode} toggleMode={toggleMode}/>
+   <div className="container" >
    <Textform />
    </div>
    <div className='container my-5'>
